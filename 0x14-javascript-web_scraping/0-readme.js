170cd47f51e0527cdf71904f58f82db86803c34c
seifaaza <seifeddine.aaza@gmail.com>
2024-04-24 11:13:07 -0700

    fix task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data);
-})
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
